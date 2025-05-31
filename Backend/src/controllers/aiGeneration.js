fd20feefc3658a81163a260fdf0701eb6b525678
Saif Amjad <saifamjad03@gmail.com>
2025-06-01 00:15:12 +0500

    Complete push

--- Backend/src/controllers/aiGeneration.js
+++ Backend/src/controllers/aiGeneration.js
@@ -1,43 +1,7 @@
-const { HfInference } = require("@huggingface/inference");
 const OpenAI = require("openai");
 
-// const hf = new HfInference("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");
-// const openai = new OpenAI({ apiKey: "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" });
-
-// const handleSuggestion = async (text, socket) => {
-//   try {
-//     const result = await hf.textGeneration({
-//       model: "tiiuae/falcon-7b-instruct",
-//       inputs: `There is a text from user which you should rephrase or suggest better sentence from it \n\nUser: ${text}\n\nAI:`,
-//       parameters: { max_new_tokens: 100, temperature: 0.7, top_p: 0.9 },
-//     });
-
-//     socket.emit("suggestion_back",result);
-//     console.log(result);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// const handleSuggestionWithOpenAI = async (text, socket) => {
-//     try {
-//         const completion = await openai.completions.create({
-//             model: "gpt-3.5-turbo",
-//             inputs: `There is a text from user which you should rephrase or suggest better sentence from it. User: ${text}`,
-//             max_tokens: 50,
-//           });
-
-//       socket.emit("suggestion_back",completion);
-//       console.log(result);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-
-// module.exports={handleSuggestion,handleSuggestionWithOpenAI}
-
 const openai = new OpenAI({
-  baseURL: "https://openrouter.ai/api/v1",
+  baseURL: process.env.BASE_URL,
   apiKey: process.env.OPENAI_API_KEY,
 });
 
@@ -45,13 +9,13 @@ const reWriteAI = async (req, res) => {
   const { content } = req.body;
   try {
     const completion = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo", 
-  messages: [
-    {
-      role: "user",
-      content: `Rewrite this text only. Do not explain anything. Just return the Rewriten text:\n\n${content}`,
-    },
-  ],
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `Rewrite this text only. Do not explain anything. Just return the Rewriten text:\n\n${content}`,
+        },
+      ],
     });
 
     return res
@@ -68,7 +32,7 @@ const summarizeAI = async (req, res) => {
   try {
     const completion = await openai.chat.completions.create({
       model: "qwen/qwen3-30b-a3b:free",
-      messages: [ 
+      messages: [
         {
           role: "system",
           content:
